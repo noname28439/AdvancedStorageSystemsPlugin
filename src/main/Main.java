d19e38c2dc4c6691b9e6f9b493a84e0a4acc5f5d
noname28439 <dohle.leon@web.de>
2022-02-06 14:14:07 +0100

    Added autoMiner

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,8 +6,10 @@ import java.util.Random;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -51,16 +53,35 @@ public class Main extends JavaPlugin{
 		plugin = this;
 		
 		
-		NamespacedKey key = new NamespacedKey(this, "storage_disc");
-		ShapedRecipe discRecipe = new ShapedRecipe(key, new ItemBuilder(Material.MUSIC_DISC_PIGSTEP, 1).setDisplayname("EmptyStorageDisk").build());
+		NamespacedKey mhkey = new NamespacedKey(this, "mining_head");
+		ShapedRecipe miningHeadRecipe = new ShapedRecipe(mhkey, new ItemBuilder(Material.GOLDEN_PICKAXE, 1).setDisplayname(ChatColor.GREEN+"MiningHead").addEnchantment(Enchantment.THORNS, 1).build());
+		miningHeadRecipe.shape("EEE", "OSO", "OSO");
+		miningHeadRecipe.setIngredient('S', Material.BLAZE_ROD);
+		miningHeadRecipe.setIngredient('E', Material.EMERALD);
+		
+		Bukkit.addRecipe(miningHeadRecipe);
+		
+		
+		NamespacedKey rmkey = new NamespacedKey(this, "range_modifier");
+		ShapedRecipe rangeModRecipe = new ShapedRecipe(rmkey, new ItemBuilder(Material.GLASS_BOTTLE, 1).setDisplayname(ChatColor.GREEN+"RangeModifier").addEnchantment(Enchantment.THORNS, 1).build());
+		rangeModRecipe.shape("CCC", "YPB", "CCC");
+		rangeModRecipe.setIngredient('Y', Material.YELLOW_STAINED_GLASS_PANE);
+		rangeModRecipe.setIngredient('B', Material.BLUE_STAINED_GLASS_PANE);
+		rangeModRecipe.setIngredient('C', Material.COPPER_BLOCK);
+		rangeModRecipe.setIngredient('P', Material.PHANTOM_MEMBRANE);
+		Bukkit.addRecipe(rangeModRecipe);
+		
+		
+		NamespacedKey dkey = new NamespacedKey(this, "storage_disc");
+		ShapedRecipe discRecipe = new ShapedRecipe(dkey, new ItemBuilder(Material.MUSIC_DISC_PIGSTEP, 1).setDisplayname("EmptyStorageDisk").build());
 		discRecipe.shape("RER", "GBG", "RER");
 		discRecipe.setIngredient('E', Material.IRON_BLOCK);
 		discRecipe.setIngredient('R', Material.REDSTONE_BLOCK);
 		discRecipe.setIngredient('G', Material.GOLD_BLOCK);
 		discRecipe.setIngredient('B', Material.LEGACY_BOOK_AND_QUILL);
-		
-		
 		Bukkit.addRecipe(discRecipe);
+		
+		
 		
 		if(plugin.getConfig().isSet("STORAGE_CONTAINERS"))
 		for(String cid : plugin.getConfig().getConfigurationSection("STORAGE_CONTAINERS").getKeys(false)) {
--- src/main/MainEventListener.java
+++ src/main/MainEventListener.java
@@ -1,6 +1,11 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -10,21 +15,30 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Dispenser;
 import org.bukkit.block.Sign;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import net.md_5.bungee.api.chat.hover.content.Item;
 import net.minecraft.world.entity.animal.EntityFox.i;
+import net.minecraft.world.inventory.Container;
 
 public class MainEventListener implements Listener {
 
@@ -105,14 +119,21 @@ public class MainEventListener implements Listener {
 	}//catch (Exception exc) { Bukkit.broadcastMessage(exc.getLocalizedMessage()); }
 	}
 	
+	
+	
 	@EventHandler
 	public void onHopperMoveItemEvent(InventoryMoveItemEvent e) {
-		
+		if(e.getItem().getItemMeta().getDisplayName().equals(ChatColor.GREEN+"MiningHead") ||
+			e.getItem().getItemMeta().getDisplayName().equals(ChatColor.GREEN+"RangeModifier"))
+			e.setCancelled(true);
 		
 		
 		//Item extracted from StorageSystem
 		Location invloc = e.getInitiator().getLocation();
+		Location destloc = e.getDestination().getLocation();
 		if(invloc==null) return;
+		if(destloc==null) return;
+		Block destinationBlock = destloc.getBlock();
 		Block initiatiorBlock = invloc.getBlock();
 		if(initiatiorBlock.getType() == Material.DROPPER) {
 			int storageSystemID = InventoryManager.validateBaseBlock(initiatiorBlock.getRelative(0, 1, 0));
@@ -128,36 +149,140 @@ public class MainEventListener implements Listener {
 		}
 		
 		
-		//trying to check if inventory is owned by a dispenser but dropper has the same amount of slots
-		if(e.getDestination().getSize()!=9)
-			return;
+		if(destinationBlock.getType() == Material.DISPENSER) {
+			//Item sorted into StorageSystem
+			ItemStack centerItem = e.getDestination().getItem(4);
+			if(centerItem != null) {
+				if(centerItem.getType() == Material.MUSIC_DISC_PIGSTEP) {
+					if(!centerItem.getItemMeta().getDisplayName().startsWith(ChatColor.DARK_PURPLE+"STORAGECELL")) {
+						int nextID = InventoryManager.storageCells.size();
+						System.out.println("Creating Storage Space "+nextID);
+						ItemMeta diskMeta = centerItem.getItemMeta();
+						diskMeta.setDisplayName(ChatColor.DARK_PURPLE+"STORAGECELL"+nextID);
+						InventoryManager.storageCells.put(nextID, new ArrayList<>());
+						centerItem.setItemMeta(diskMeta);
+					}
+					
+					int id = Integer.valueOf(centerItem.getItemMeta().getDisplayName().replace(ChatColor.DARK_PURPLE+"STORAGECELL", ""));
+					if(InventoryManager.storageCells.get(id) == null)
+						InventoryManager.storageCells.put(id, new ArrayList<>());
+					
+					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, ()->{
+						e.getDestination().remove(e.getItem());
+						InventoryManager.addItemStackToStorageCell(id, e.getItem().clone());
+					});
+				}
+			}
+		}
 		
-		//Item sorted into StorageSystem
-		ItemStack storageDisk = e.getDestination().getItem(4);
-		if(storageDisk != null) {
-			if(storageDisk.getType() == Material.MUSIC_DISC_PIGSTEP) {
-				if(!storageDisk.getItemMeta().getDisplayName().startsWith(ChatColor.DARK_PURPLE+"STORAGECELL")) {
-					int nextID = InventoryManager.storageCells.size();
-					System.out.println("Creating Storage Space "+nextID);
-					ItemMeta diskMeta = storageDisk.getItemMeta();
-					diskMeta.setDisplayName(ChatColor.DARK_PURPLE+"STORAGECELL"+nextID);
-					InventoryManager.storageCells.put(nextID, new ArrayList<>());
-					storageDisk.setItemMeta(diskMeta);
+		
+	}
+	
+	
+	@EventHandler
+    public void onDispenseItem(BlockDispenseEvent e) {//WARNING CRAPPY CODE!
+		Block initiatiorBlock = e.getBlock();
+		if(initiatiorBlock==null) return;
+	    if(initiatiorBlock.getType() == Material.DISPENSER) {
+	    	Inventory dispenserInv = ((InventoryHolder)(initiatiorBlock.getState())).getInventory();
+	    	ItemStack baseItem = dispenserInv.getItem(4);
+	    	ItemStack modifier = dispenserInv.getItem(7);
+	    	if(e.getItem().getItemMeta().getDisplayName().equals(ChatColor.GREEN+"MiningHead") || baseItem.getItemMeta().getDisplayName().equals(ChatColor.GREEN+"MiningHead")) {
+				e.setCancelled(true);
+				int rad = 0;
+				
+				if(modifier==null) {
+					if(e.getItem().getItemMeta().getDisplayName().equals(ChatColor.GREEN+"RangeModifier"))
+						rad+=e.getItem().getAmount();
+				}else if(modifier.getItemMeta().getDisplayName().equals(ChatColor.GREEN+"RangeModifier"))
+					rad+=modifier.getAmount();
+					
+				if(rad>5)
+					rad=5;
+				
+				if (initiatiorBlock.getBlockData() instanceof Directional) {
+					Block toBreak = null;
+				    BlockFace facing = ((Directional) initiatiorBlock.getBlockData()).getFacing();
+				    int dist = 0;
+				    mainloop: while(dist++<=30) {
+				    	for(int xof = -rad; xof<=rad; xof++) {
+				    		for(int yof = -rad; yof<=rad; yof++) {
+				    			for(int zof = -rad; zof<=rad; zof++) {
+						    		int xofs = xof;
+						    		int yofs = yof;
+						    		int zofs = zof;
+						    		if(facing.getModX()!=0) {xof=rad+999; xofs = 0;}
+						    		if(facing.getModY()!=0) {yof=rad+999; yofs = 0;}
+						    		if(facing.getModZ()!=0) {zof=rad+999; zofs = 0;}
+						    		
+						    		Block currentCheck = initiatiorBlock.getRelative(facing, dist).getRelative(xofs, yofs, zofs);
+							    	//System.out.println(locationToString(currentCheck.getLocation())+" --> "+currentCheck.getType());
+					    			if(currentCheck.getType() == Material.REDSTONE_WIRE ||
+					    			currentCheck.getType() == Material.BEDROCK ||
+					    			currentCheck.getType() == Material.BARRIER ||
+			    					currentCheck.getType() == Material.TORCH ||
+			    					currentCheck.getType() == Material.CHEST ||
+			    					currentCheck.getType() == Material.BARREL ||
+			    					currentCheck.getType() == Material.GLASS ||
+									currentCheck.getType() == Material.AIR ||
+									currentCheck.isLiquid()) {
+					    				continue;	//Invalid block --> Ignore
+					    			}else {
+					    				toBreak = currentCheck;
+					    				break mainloop;
+					    			}
+						    		
+						    	}
+					    	}
+				    	}
+				    	
+		    			
+				    	
+				    }
+				    	
+				    
+				    Block container = initiatiorBlock.getRelative(facing.getOppositeFace());
+				    if(toBreak == null) return;	//No Block in Range
+				    if(container == null) return; //No container behind
+				    
+			    	
+				    Collection<ItemStack> drops = toBreak.getDrops();
+				    toBreak.setType(Material.AIR);
+				    
+				    toBreak.getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, toBreak.getLocation().clone().add(0.5, 0.5, 0.5), 1, 0, 0, 0, 0);
+				    for(int i = 0; i<3; i++) {
+				    	Random rand = new Random();
+				    	Location inBLoc = toBreak.getLocation().clone().add(Float.valueOf(rand.nextInt(100))/100, Float.valueOf(rand.nextInt(100))/100, Float.valueOf(rand.nextInt(100))/100);
+				    	if(rand.nextInt(3)==0)
+				    		toBreak.getWorld().spawnParticle(Particle.DRIP_WATER, inBLoc, 10);
+				    	else
+				    		toBreak.getWorld().spawnParticle(Particle.DRIP_LAVA, inBLoc, 10);
+				    }
+				    
+				    if(container.getState() instanceof InventoryHolder) {
+				    	Inventory blockinv = ((InventoryHolder)(container.getState())).getInventory();
+					    for(ItemStack cis : drops)
+					    	blockinv.addItem(cis);
+				    }
+				    
+				    
+				    
 				}
 				
-				int id = Integer.valueOf(storageDisk.getItemMeta().getDisplayName().replace(ChatColor.DARK_PURPLE+"STORAGECELL", ""));
-				if(InventoryManager.storageCells.get(id) == null)
-					InventoryManager.storageCells.put(id, new ArrayList<>());
-				
-				Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, ()->{
-					e.getDestination().remove(e.getItem());
-					InventoryManager.addItemStackToStorageCell(id, e.getItem().clone());
-				});
-				
-				
-				
-				
 			}
+	    }
+	}
+		
+	
+	
+	@EventHandler
+	public void onChatSend(AsyncPlayerChatEvent e) {
+		if(e.getMessage().contains("@sevra relexe")) {
+			Bukkit.getScheduler().runTask(Main.plugin, () -> {
+				e.getPlayer().sendMessage(ChatColor.GREEN+"sevra traves relexa...");
+				Bukkit.reload();
+			});
+			e.setCancelled(true);
 		}
 	}
 	
